10036c44036498d33ca6c6e6c39491dcb0891adb
Waqar <mwaqar29@gmail.com>
2020-04-02 00:28:01 +0530

    swipable offers implemented

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesModel } from '../places.model';
 import { PlacesService } from '../places.service';
+import { IonItemSliding } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-offers',
@@ -11,7 +13,7 @@ export class OffersPage implements OnInit {
 
   offers: PlacesModel[];
 
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService, private router: Router) { }
 
   ngOnInit() {
     console.log('Offers loaded');
@@ -19,4 +21,9 @@ export class OffersPage implements OnInit {
     console.log(this.offers);
   }
 
+  onEdit(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'offers', 'edit', offerId]);
+    console.log(offerId);
+  }
 }
